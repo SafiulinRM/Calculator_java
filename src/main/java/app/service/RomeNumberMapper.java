4ccb0f8ba718412aa033ce08c518ce65de05c5fa
safiulinrm <zloyangel666999@gmail.com>
2022-12-07 14:39:07 +0300

    feat "добавил класс преобразования римских чисел"

---
+++ src/main/java/app/service/RomeNumberMapper.java
@@ -0,0 +1,33 @@
+package app.service;
+
+import java.util.Arrays;
+
+class RomeNumberMapper {
+    public static String[] romeNumbers = {"пустая клетка", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"
+    };
+    public static String[] toArabicOperation(String[] values) {
+        values[0] = toArabicNumber(values[0]);
+        values[2] = toArabicNumber(values[2]);
+        return values;
+    }
+
+    private static String toArabicNumber(String value) {
+        int index = Arrays.asList(romeNumbers).indexOf(value);
+        if (index == -1){
+            throw new RuntimeException("Используются одновременно разные системы счисления");
+        }
+        return String.valueOf(index);
+    }
+}
